eadbf8bcd385ece64c1bd91e37491c88a05ef558
lemoonchild <ncastrom21@gmail.com>
2024-05-28 20:36:33 -0600

    fix: fixed the content in timeline

--- portafolio-web/src/pages/experience/index.jsx
+++ portafolio-web/src/pages/experience/index.jsx
@@ -10,20 +10,31 @@ const Experience = () => {
     <VerticalTimeline>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
+        date="January 2024- Present"
+        iconStyle={{ background: '#B6BBC4', color: '#161a30' }}
+        icon={<FaBriefcaseMedical />}
+      >
+        <h3 className="vertical-timeline-element-title">Full Stack Developer</h3>
+        <h4 className="vertical-timeline-element-subtitle">Sanitas Project</h4>
+        <p className="content-text">
+          Engineered a medical record management system for Universidad del Valle de Guatemala.
+          Enhanced data collection and processing for patient history, symptoms, and healthcare
+          recommendations, focusing on a seamless interface for both students and medical staff.
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        className="vertical-timeline-element--work"
         date="March 2024 - April 2024"
         iconStyle={{ background: '#B6BBC4', color: '#161a30' }}
         icon={<FaCat />}
       >
-        <h3 className="vertical-timeline-element-title">Front End Developer</h3>
+        <h3 className="vertical-timeline-element-title">Full Stack Developer</h3>
         <h4 className="vertical-timeline-element-subtitle">The Cozy Whiskers</h4>
         <p className="content-text">
-          Developed a management system for "The Cozy Whiskers," a cat café, utilizing technologies
-          such as React, Vite, Node.js, PostgreSQL, and JavaScript, deployed on Vercel. This system
-          empowers restaurant employees to create invoices, manage and review orders, and
-          efficiently track occupied tables. The project significantly optimized restaurant
-          operations, enhancing service efficiency and customer satisfaction. Took on the role of
-          Front End developer, focusing on crafting a user-friendly and intuitive interface for the
-          system, ensuring a seamless experience for users.
+          Developed a comprehensive management system for "The Cozy Whiskers," a cat café. Utilized
+          React, Vite, Node.js, and PostgreSQL, focusing on an intuitive user interface and backend
+          logic to streamline operations, improve service efficiency, and enhance customer
+          satisfaction.
         </p>
       </VerticalTimelineElement>
 
@@ -35,33 +46,11 @@ const Experience = () => {
       >
         <h3 className="vertical-timeline-element-title">Full Stack Developer</h3>
         <h4 className="vertical-timeline-element-subtitle">The Evangelion Blog</h4>
-
         <p className="content-text">
-          Created a blog project focused on the anime "Evangelion." Users can engage with various
-          functionalities such as reading, creating, updating, and deleting posts based on their
-          permissions. The project aimed to enhance the interaction within the Evangelion fan
-          community through an appealing interface and real-time interaction features. Took on the
-          roles of both Front End and Back End developer, overseeing the entire site's creation
-          process.
-        </p>
-      </VerticalTimelineElement>
-
-      <VerticalTimelineElement
-        className="vertical-timeline-element--work"
-        date="September 2023 - Present"
-        iconStyle={{ background: '#B6BBC4', color: '#161a30' }}
-        icon={<FaBriefcaseMedical />}
-      >
-        <h3 className="vertical-timeline-element-title">Front end Developer</h3>
-        <h4 className="vertical-timeline-element-subtitle">Sanitas Project</h4>
-
-        <p className="content-text">
-          Developed a project for Universidad del Valle de Guatemala focused on managing medical
-          records and collecting students' medical data. The system streamlines the process of
-          recording patients' medical history, symptoms, and recommendations during consultations
-          with healthcare professionals. As a Full Stack Developer, contributed to both Front End
-          and Back End development, ensuring efficient data management and user-friendly interface
-          for healthcare providers.
+          Created an interactive blog for "Evangelion" anime enthusiasts. Designed to foster
+          community interaction, the platform features functionalities for creating, reading,
+          updating, and deleting content, while ensuring secure user access and real-time
+          communication.
         </p>
       </VerticalTimelineElement>
     </VerticalTimeline>
